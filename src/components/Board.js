b59fb30a2660ee37f5958e0280a24989d02b4a7f
Ty <thelacycode@gmail.com>
2018-02-06 21:09:36 -0500

    Initial commit

---
+++ src/components/Board.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/Board.css';
+
+class Board extends React.Component {
+  render() {
+    return(
+      <div className='game-board'>
+        <p className="game-text">Game Board</p>
+      </div>
+    )
+}
+}
+
+export default Board;
